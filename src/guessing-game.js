352c44d37e6b5f0ab654043fec294d3795204b5c
Dzmitry Tsiareshchanka <dima.tereshchenko.96@mail.ru>
2023-03-12 17:54:16 +0400

    feat: add GuessingGame class with solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.array = [];
+        this.middleNum;
+    }
 
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++) {
+            this.array.push(i);
+        }
     }
 
     guess() {
-
+        this.middleNum = this.array[Math.floor(this.array.length / 2)];
+        return this.middleNum;
     }
 
     lower() {
-
+        this.array = this.array.filter(el => el <= this.middleNum);
     }
 
     greater() {
-
+        this.array = this.array.filter(el => el >= this.middleNum);
     }
 }
 
